3269c69218cecfa34f3948cc88f3b9391bba1b77
rahmagamal <55654504+rahmagamal@users.noreply.github.com>
2020-12-08 03:29:44 +0200

    first commit

---
+++ js/about.a72a8665.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(t,n,e){"use strict";e.r(n);var a=function(){var t=this,n=t.$createElement,e=t._self._c||n;return e("div",{staticClass:"about"},[t._v(" hhhhhhhhh ")])},h=[],s=e("2877"),u={},l=Object(s["a"])(u,a,h,!1,null,null,null);n["default"]=l.exports}}]);
+//# sourceMappingURL=about.a72a8665.js.map
